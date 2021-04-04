25d22a92eba3aaed6ffaa3895d6a441e8c7c3f2d
hamza campo <mdhamzaca@gmail.com>
2021-04-04 18:05:27 +0300

    NPM Scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});   
+    $("#carousel-Button").click(function(){
+        if($("#carousel-Button").children("span").hasClass('fa-pause')){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-Button").children("span").removeClass('fa-pause');
+            $("#carousel-Button").children("span").addClass('fa-play');
+        }
+        else if($("#carousel-Button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-Button").children("span").removeClass('fa-play');
+            $("#carousel-Button").children("span").addClass('fa-pause'); 
+        }
+        
+        
+    });
+    $("#reserve-table-btn").click(function(){
+        $("#ReserveModal").modal('toggle');
+    });
+    $("#login-modal-btn").click(function(){
+        $("#LoginModal").modal('toggle');
+    });
+});
